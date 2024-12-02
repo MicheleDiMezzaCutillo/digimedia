0ee76eebb48752e3d575dda23cf648efbb0a6a00
MikeDmC <micheleilpro@libero.it>
2024-12-02 20:47:14 +0100

    primo commit

---
+++ src/main/java/it/mikedmc/digimedia/DigimediaApplication.java
@@ -0,0 +1,13 @@
+package it.mikedmc.digimedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigimediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigimediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/mikedmc/digimedia/DigimediaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.mikedmc.digimedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigimediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
